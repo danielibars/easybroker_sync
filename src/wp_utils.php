98d7402c8fda5b5aa7a910975db5b85a346ecfab
Daniel Ibars <ibars.daniel@gmail.com>
2022-03-21 20:42:23 -0600

    correccion de errores y filtro por tag

--- src/wp_utils.php
+++ src/wp_utils.php
@@ -1,7 +1,7 @@
 <?php
 
 function slugify($string){
-    $slug = strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $string)));
+    $slug = strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',$string))));
     return $slug;
 }
 
@@ -9,23 +9,46 @@ function location_ids($location_string){
 
     error_log("UBICACION: $location_string");
     $locations = explode(", ", $location_string);
-    $parent=0;
+    $the_parent=0;
     $ids = array();
     foreach(array_reverse($locations) as $location){
         error_log("beforetrim: $location");
-        $parent = wp_insert_term(
+
+       
+
+        $parent = (array) wp_insert_term(
             $location,   // the term 
             'property_location', // the taxonomy
             array(
                 'description' => $location,
                 'slug'        => slugify($location),
-                'parent'      => $parent,
+                'parent'      => $the_parent,
             )
         );
-        error_log("parentid: $parent");
-        $ids[]=$parent;
+        if (array_key_exists('term_id',$parent)){
+            error_log("el term fue creado con el id: ".$parent['term_id']);
+            $the_parent=$parent['term_id'];
+        }elseif(isset($parent['error_data']['term_exists'])){
+            error_log("el term ya existía con el id: ".$parent['error_data']['term_exists']);
+            $the_parent=$parent['error_data']['term_exists'];
+        }
+        error_log("parent: ".print_r($parent['term_id'], true));
+        error_log("the_parent: ".print_r($the_parent, true));
+        $ids[]=$the_parent;
     }
-    error_log("ids: ".print_r($ids));
+    error_log("ids: ".print_r($ids, true));
     return $ids;
 
+}
+
+
+function find_property_by_meta_field($post_type, $meta_key, $meta_value){
+    $search_args = array(
+        'numberposts'=>1,
+        'post_type'=>$post_type,
+        'meta_key'=>$meta_key,
+        'meta_value'=>$meta_value
+    );
+    $posts = get_posts($search_args);
+    return $posts;
 }
